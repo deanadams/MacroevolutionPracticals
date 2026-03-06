607574a5edf0c5b6f8fb9dab783c5d0eb2288cf2
Dean Adams <dcadams@iastate.edu>
2026-03-06 12:42:33 -0600

    14

--- 14-Biogeography/scripts/Biogeography.r
+++ 14-Biogeography/scripts/Biogeography.r
@@ -31,5 +31,52 @@ bgb_run <- define_BioGeoBEARS_run(
 bgb_run$geogfn <- "../data/attine-distribution-data.txt"
 check_BioGeoBEARS_run(bgb_run)
 DEC.fit <- bears_optim_run(bgb_run)
+
+## ----res_dec ----
 DEC.fit$optim_result
 
+## ----plot_dec ----
+layout(matrix(1:2, 1,2), widths=c(0.2,0.8))
+par(mar=c(4.1,0.1,1.3,0.1),cex=0.8)
+plot_BioGeoBEARS_results(DEC.fit,
+    analysis_titletxt = "DEC model",
+    plotlegend = TRUE, tipcex = 0.4,statecex = 0.4)
+
+## ----run_decj ----
+dstart <- DEC.fit$outputs@params_table["d","est"]
+estart <- DEC.fit$outputs@params_table["e","est"]
+jstart <- 0.0001
+
+bgb_run$BioGeoBEARS_model_object@params_table["d",
+        "init"] <- dstart
+bgb_run$BioGeoBEARS_model_object@params_table["d",
+        "est"] <- dstart
+bgb_run$BioGeoBEARS_model_object@params_table["e",
+        "init"] <- estart
+bgb_run$BioGeoBEARS_model_object@params_table["e",
+        "est"] <- estart
+bgb_run$BioGeoBEARS_model_object@params_table["j",
+        "type"] <- "free"
+bgb_run$BioGeoBEARS_model_object@params_table["j",
+        "init"] <- jstart
+bgb_run$BioGeoBEARS_model_object@params_table["j",
+        "est"] <- jstart
+check_BioGeoBEARS_run(bgb_run)
+DEC_J.fit <- bears_optim_run(bgb_run)
+
+## ----res_decj ----
+DEC_J.fit$optim_result
+
+## ----plot_decj ----
+layout(matrix(1:2, 1,2), widths=c(0.2,0.8))
+par(mar=c(4.1,0.1,1.3,0.1),cex=0.8)
+plot_BioGeoBEARS_results(DEC_J.fit,
+      analysis_titletxt = "DEC+J model",
+      plotlegend = TRUE, tipcex = 0.4,statecex = 0.4)
+
+## ----model_compare ----
+logL.DEC <- get_LnL_from_BioGeoBEARS_results_object(DEC.fit)
+logL.DECJ <- get_LnL_from_BioGeoBEARS_results_object(DEC_J.fit)
+AIC.table <- AICstats_2models(logL.DECJ,logL.DEC, numparams1 = 3,
+                              numparams2 = 2)
+print(AIC.table)
